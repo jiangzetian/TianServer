bcea108cc6b0f789ed15792690fab84558317b7b
蒋泽天 <zetian@neurotech-solutions.com>
2020-11-23 14:09:19 +0800

    文章管理功能开发（分页查询）

--- app/api/controller/Article.php
+++ app/api/controller/Article.php
@@ -11,9 +11,27 @@ use think\Request;
 
 class Article extends Auth
 {
-    public function index()
+    //分页查询
+    public function page()
     {
-        //
+        //权限验证
+        $checkArr = $this->checkToken();
+        if ($checkArr['code']!==1){return $checkArr;}
+        //获取请求数据
+        $createArr = input('get.');
+        //验证器
+        validate(ArticleValidate::class)->scene('page')->check($createArr);
+        //查询
+        $sqlData = articleModel::where([
+            ['title', 'like', '%'.$createArr['title'].'%'],
+        ])
+            ->order('date', 'desc')
+            ->paginate([
+                'list_rows'=> $createArr['pageSize'],
+                'page' => $createArr['currentPage']
+            ])
+            ->toArray();
+        return common\success(200,'查询文章列表成功',$sqlData);
     }
     //新增
     public function create()
@@ -30,26 +48,5 @@ class Article extends Auth
         $sqlData = articleModel::create($articleArr,['id','title','desc','category','url','date','content','html']);
         //返回
         return common\success(200,'添加文章分类成功',$sqlData);
-    }
-
-    public function save(Request $request)
-    {
-        //
-    }
-    public function read($id)
-    {
-        //
-    }
-    public function edit($id)
-    {
-        //
-    }
-    public function update(Request $request, $id)
-    {
-        //
-    }
-    public function delete($id)
-    {
-        //
     }
 }
